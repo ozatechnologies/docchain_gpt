1e7b379122617b6baa32e4d4e26dd70f53e7d04b
ozatechnologies <100591237+ozatechnologies@users.noreply.github.com>
2023-05-16 23:52:55 +0530

    Update faq.py

--- knowledge_gpt/components/faq.py
+++ knowledge_gpt/components/faq.py
@@ -6,17 +6,21 @@ def faq():
     st.markdown(
         """
 # FAQ
-## How does KnowledgeGPT work?
+## How does DocChaineGPT by syst work?
 When you upload a document, it will be divided into smaller chunks 
 and stored in a special type of database called a vector index 
 that allows for semantic search and retrieval.
 
-When you ask a question, KnowledgeGPT will search through the
+When you ask a question, DCGT will search through the
 document chunks and find the most relevant ones using the vector index.
 Then, it will use GPT3 to generate a final answer.
 
+## What is Used in this build ?
+1. SYST LLM (SlliaAM 1.1)
+2. OpenAI LLM (ChatGPT 3)
+
 ## Is my data safe?
-Yes, your data is safe. KnowledgeGPT does not store your documents or
+Yes, your data is safe. DCGPT does not store your documents or
 questions. All uploaded data is deleted after you close the browser tab.
 
 ## Why does it take so long to index my document?
@@ -31,15 +35,15 @@ the chunk number on that page. For DOCS and TXT documents,
 the first number is set to 1 and the second number is the chunk number.
 
 ## Are the answers 100% accurate?
-No, the answers are not 100% accurate. KnowledgeGPT uses GPT-3 to generate
+No, the answers are not 100% accurate. DCGPT uses GPT-3 to generate
 answers. GPT-3 is a powerful language model, but it sometimes makes mistakes 
-and is prone to hallucinations. Also, KnowledgeGPT uses semantic search
+and is prone to hallucinations. Also, DCGPT uses semantic search
 to find the most relevant chunks and does not see the entire document,
 which means that it may not be able to find all the relevant information and
 may not be able to answer all questions (especially summary-type questions
 or questions that require a lot of context from the document).
 
-But for most use cases, KnowledgeGPT is very accurate and can answer
+But for most use cases, DCGPT is very accurate and can answer
 most questions. Always check with the sources to make sure that the answers
 are correct.
 """
